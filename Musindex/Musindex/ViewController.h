ea6a2617bdce6ffe66798f38e166a8f8ee77e33f
Danilo Soares Aliberti <dsaliberti@gmail.com>
2016-01-11 22:05:05 -0200

    XCode project created AppIcon added

---
+++ Musindex/Musindex/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Musindex
+//
+//  Created by Danilo Soares Aliberti on 1/11/16.
+//  Copyright © 2016 dsaliberti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Musindex/Musindex/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Musindex
+//
+//  Created by Danilo Soares Aliberti on 1/11/16.
+//  Copyright © 2016 dsaliberti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

a40617f46706db75d23261e171bdd5883671c1fe
Danilo Soares Aliberti <dsaliberti@gmail.com>
2016-01-13 00:26:55 -0200

    UITableView: data source and delegate implemented; reload data called on main thread after a successful result; trackName added to the model;

--- Musindex/Musindex/ItunesResultItem.m
+++ Musindex/Musindex/ItunesResultItem.m
@@ -14,6 +14,7 @@
     if (self = [super init]) {
         self.wrapperType =  dictionary[@"wrapperType"];
         self.artistName =   dictionary[@"artistName"];
+        self.trackName =    dictionary[@"trackName"];
     }
     return self;
 }
--- Musindex/Musindex/SearchViewController.m
+++ Musindex/Musindex/SearchViewController.m
@@ -12,15 +12,31 @@
 @interface SearchViewController ()
 @property (strong, nonatomic) IBOutlet UISearchBar *searchBar;
 @property (strong, nonatomic) ItunesService *service;
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
+@property (strong, nonatomic) NSArray <ItunesResultItem> *searchResults;
+
 @end
 
 @implementation SearchViewController
+
+#pragma mark UIViewController Lifecycle
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.searchBar.delegate = self;
     self.service = [[ItunesService alloc] init];
+    self.searchResults = (NSArray <ItunesResultItem> *) @[];
+    self.tableView.tableHeaderView = [UIView new];
+    self.tableView.tableFooterView = [UIView new];
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
 }
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+#pragma mark SearchBar Delegate
 
 -(void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
     
@@ -28,16 +44,52 @@
     
     [self.service getItunesResultsWithQuery:queryString
                          andSuccessCallback:^(NSArray<ItunesResultItem> *response) {
-                             NSLog(@"%@", [response valueForKey:@"artistName"] );
+                             
+                             self.searchResults = response;
+                             
+                             dispatch_async(dispatch_get_main_queue(), ^{
+                                 [self.tableView reloadData];
+                                 [self.view endEditing:YES];
+                             });
     }
                                    andError:^(NSError *errorResponse) {
                                        NSLog(@"%@", errorResponse);
     }];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
+#pragma mark UITableView Delegate and DataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
 }
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.searchResults.count;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 60;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *cellIdentifier = @"cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellIdentifier];
+    }
+    
+    ItunesResultItem *item = [self.searchResults objectAtIndex:indexPath.row];
+    
+    cell.textLabel.text = item.artistName;
+    cell.detailTextLabel.text = item.trackName;
+    
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+}
+
 
 
 @end
